6fc93e50d29ec051d74835d7a030e4ad2a3fed56
Chester Don Valencerina <vale0192@algonquinlive.com>
2025-05-27 17:37:58 -0400

    fixed Images

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ export const HeroSection = () => {
       className="relative min-h-screen flex flex-col items-center justify-center px-4"
     >
       <img
-        src="/public/projects/backImg.png"
+        src="/projects/backImg.png"
         alt=""
         className="absolute right-1.5 md:top-4 md:w-2xl w-1/2 top-3/5"
       />
