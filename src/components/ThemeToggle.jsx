a031b51d41f44f53387bf23f2183181dfd27442f
Chester Don Valencerina <vale0192@algonquinlive.com>
2025-05-28 23:14:54 -0400

    dark mode default

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,28 +3,27 @@ import { Sun, Moon } from "lucide-react";
 import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
     if (storedTheme === "dark") {
       document.documentElement.classList.add("dark");
-      setIsDarkMode(true);
+      setIsDarkMode(false);
     } else {
       document.documentElement.classList.remove("dark");
-      setIsDarkMode(false);
+      setIsDarkMode(true);
     }
   }, []);
 
   const toggleTheme = () => {
     if (isDarkMode) {
-      {/** */}
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      setIsDarkMode(true);
-    } else {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("theme", "light");
       setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
     }
   };
 
