32ee57e87c8c75cffd2252fbe68e8171a31de80f
Chester Don Valencerina <vale0192@algonquinlive.com>
2025-06-10 15:20:55 -0400

    Added a Orb to the hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,6 @@
 import { ArrowDown } from "lucide-react";
 import { RevealOnScroll } from "./RevealOnScroll";
+import Orb from "./Orb";
 
 export const HeroSection = () => {
   return (
@@ -12,7 +13,24 @@ export const HeroSection = () => {
         alt=""
         className="absolute right-1.5 md:top-4 md:w-2xl w-1/2 top-3/5"
       />
+
       <RevealOnScroll>
+        <div
+          style={{
+            width: "100%",
+            height: "800px",
+            position: "absolute",
+            zIndex: -1,
+            top: "-280px",
+          }}
+        >
+          <Orb
+            hoverIntensity={0.5}
+            rotateOnHover={true}
+            hue={235}
+            forceHoverState={false}
+          />
+        </div>
         <div className="container max-w-4xl mx-auto text-center z-10">
           <div className="space-y-6">
             <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
